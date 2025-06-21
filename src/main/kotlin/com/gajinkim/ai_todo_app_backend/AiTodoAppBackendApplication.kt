144b5dd6fb8e35569a69c8bd6415bc0e41e95946
GaJin Kim <gajin2000@gmail.com>
2025-06-21 13:28:40 -0500

    initalizer spring

---
+++ src/main/kotlin/com/gajinkim/ai_todo_app_backend/AiTodoAppBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.gajinkim.ai_todo_app_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AiTodoAppBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<AiTodoAppBackendApplication>(*args)
+}
